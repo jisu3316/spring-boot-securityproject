669a597fac991ec160ea23b7fb33d6c3d17fe909
jisu3316 <jisu3316@naver.com>
2022-06-25 15:32:18 +0900

    프로젝트 생성

---
+++ src/test/java/com/jisu/securityproject/SecurityprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jisu.securityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
